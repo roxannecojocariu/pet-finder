aeff8f8a920a9feef49eb688e297430f4fc11816
Roxanne Cojocariu <rmcojocariu@gmail.com>
2019-03-10 16:47:48 -0400

    add pets view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pets
 
   namespace :api do
     namespace :v1 do
