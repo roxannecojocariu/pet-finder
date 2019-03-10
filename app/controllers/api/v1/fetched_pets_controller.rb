4d95150d77ca66c145a0e004f0cf87d7c40d8f61
Roxanne Cojocariu <rmcojocariu@gmail.com>
2019-03-09 20:32:12 -0500

    parse through data properly

--- app/controllers/api/v1/fetched_pets_controller.rb
+++ app/controllers/api/v1/fetched_pets_controller.rb
@@ -1,17 +1,11 @@
 require 'net/http'
+
 require 'pry'
 
 class Api::V1::FetchedPetsController < ApplicationController
 
   def index
-    uri = URI("http://api.petfinder.com/pet.find?key=#{ENV["PET_FINDER_API_KEY"]}&animal=dog&location=08093&format=json")
-
-    Net::HTTP.start(uri.host, uri.port) do |http|
-      request = Net::HTTP::Get.new uri
-      binding.pry
-      response = http.request request
-
-      render json: response.body
-    end
+    pets = FetchedPet.new
+    render json: pets.pet_finder_search
   end
 end
